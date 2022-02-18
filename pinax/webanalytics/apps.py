4f737002c7682f6db2d7366119aa8bcade2ed575
John Heerman <john.heerman@agcocorp.com>
2022-02-18 15:57:43 -0600

    Update app label

--- pinax/webanalytics/apps.py
+++ pinax/webanalytics/apps.py
@@ -5,5 +5,5 @@ from django.utils.translation import ugettext_lazy as _
 class AppConfig(BaseAppConfig):
 
     name = "pinax.webanalytics"
-    label = "pinax-webanalytics"
+    label = "pinax_webanalytics"
     verbose_name = _("Pinax Web Analytics")
